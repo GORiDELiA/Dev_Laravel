d3fe86657b329ea3a47ddfc8442f703fb32b316c
GORiDELiA <general.asano@gmail.com>
2021-04-25 22:16:01 +0900

    remove sensitive data

--- db/init.d/user_01_Delia.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'Delia'@'localhost' IDENTIFIED BY 'docker';
-GRANT ALL PRIVILEGES ON *.* TO 'Delia'@'localhost';
-
-CREATE USER 'Delia'@'%' IDENTIFIED BY 'docker';
-GRANT ALL PRIVILEGES ON *.* TO 'Delia'@'%';
